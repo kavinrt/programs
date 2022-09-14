481a008a40e8ef3f3d8329b2f5ed6e4d6912a83c
kavinrt <kavinvenkatesan16@gmail.com>
2022-09-14 17:40:23 +0530

    using char array

---
+++ Ascii.java
@@ -0,0 +1,23 @@
+
+public class Ascii
+{
+
+public static void main(String arg[])
+
+{
+   char ch[] = {'A','B','C'};
+     {
+       for(int i=0; i<ch.length; i++){
+
+        int a= ch[i];
+            a= a+32;
+        ch[i]=(char)a;
+             
+
+
+          System.out.println(ch[i]);
+}
+
+}        
+}
+}
